34cdc8fe96d2b2c5fcf74fc1a022dd6b4cfb8e6b
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-01-25 17:12:22 -0300

    prototype method examplo

--- CreationalPatterns/PrototypeMethod/index.php
+++ CreationalPatterns/PrototypeMethod/index.php
@@ -1,3 +1,59 @@
 <?php
 
 namespace RefactoringGuru\Prototype\RealWorld;
+
+class Page {
+    private $title;
+    private $body;
+
+    private $author;
+    private $comments = [];
+    private $date;
+
+    public function __construct(string $title, string $body, Author $author)
+    {
+        $this->title = $title;
+        $this->body = $body;
+        $this->author = $author;
+        $this->author->addToPage($this);
+        $this->date = new \DateTime();
+    }
+
+    public function addComment(string $comment): void {
+        $this->comments[] = $comment;
+    }
+
+    public function __clone()
+    {
+        $this->title = "Copy of ". $this->title;
+        $this->author->addToPage($this);
+        $this->comments = [];
+        $this->date = new \DateTime();
+    }
+}
+
+class Author {
+    private $name;
+    private $pages = [];
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addToPage(Page $page): void {
+        $this->pages[] = $page;
+    }
+}
+
+function clientCode() {
+    $author = new Author("Eduardo Gomes 42");
+    $page = new Page("viagem do dia", "mantenha a calma e vá", $author);
+
+    $page->addComment("Boa viagem, obrigado!");
+    $draft = clone $page;
+    echo "Dump of the clone. Note that the author is now referencing two object";
+    print_r($draft);
+}
+
+clientCode();
