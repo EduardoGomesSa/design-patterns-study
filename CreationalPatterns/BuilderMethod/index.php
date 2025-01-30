6a4bde38b0a03e3eb65057803a86390f6cd7b168
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-01-30 09:32:51 -0300

    Finalização do exemplo de Builder

--- CreationalPatterns/BuilderMethod/index.php
+++ CreationalPatterns/BuilderMethod/index.php
@@ -73,3 +73,22 @@ class PostgresQueryBuilder extends MysqlQueryBuilder {
         return $this;
     }
 }
+
+function clientCode(SQLQueryBuilder $queryBuilder) {
+    $query = $queryBuilder 
+        ->select("users", ["name", "email", "password"])
+        ->where("age", 18, ">")
+        ->where("age", 30, "<")
+        ->limit(10, 20)
+        ->getSQL();
+
+    echo $query;
+}
+
+echo "Testando o query Builder do MySQL:\n";
+clientCode(new MysqlQueryBuilder());
+echo "\n";
+
+echo "Testando o query Builder do PostgresSQL:\n ";
+clientCode(new PostgresQueryBuilder());
+echo "\n";
