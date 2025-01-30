65c323e26839a87f66cb5a6365ad338c094b8a3e
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-01-30 01:09:06 -0300

    criação da interface SQLQueryBuilder

--- CreationalPatterns/BuilderMethod/index.php
+++ CreationalPatterns/BuilderMethod/index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo "Metodo Builder";
+namespace RefactoringGuru\Builder\RealWorld;
+
+interface SQLQueryBuilder {
+    public function select(string $table, array $fields) : SQLQueryBuilder;
+
+    public function where(string $field, string $value, string $operator = '=');
+
+    public function limit(int $start, int $offset) : SQLQueryBuilder;
+
+    public function getSQL() : string;
+}
