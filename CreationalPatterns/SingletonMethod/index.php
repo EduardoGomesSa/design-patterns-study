1419fc004150e96c64f0f4a5c16a147e2b9bade0
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-02-08 23:51:22 -0300

    criação da class Singleton

--- CreationalPatterns/SingletonMethod/index.php
+++ CreationalPatterns/SingletonMethod/index.php
@@ -4,4 +4,22 @@ namespace RefactoringGuru\Singleton\RealWorld;
 
 class Singleton {
     private static $instances = [];
+
+    protected function __construct() {}
+
+    protected function __clone(){}
+
+    public function __wakeup() {
+        throw new \Exception("Cannot unserialize singleton");
+    }
+
+    public static function getInstance() {
+        $subclass = static::class;
+
+        if(!isset(self::$instances[$subclass])) {
+            self::$instances[$subclass];
+        }
+
+        return self::$instances[$subclass];
+    }
 }
