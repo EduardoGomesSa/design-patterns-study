84c19c2e76b5b89f0cc3a53fce92b658928e00ed
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-02-09 00:12:21 -0300

    Criação da classe Config e Logger

--- CreationalPatterns/SingletonMethod/index.php
+++ CreationalPatterns/SingletonMethod/index.php
@@ -23,3 +23,36 @@ class Singleton {
         return self::$instances[$subclass];
     }
 }
+
+class Logger extends Singleton {
+    private $fileHandle;
+
+    protected function __construct() {
+        $this->fileHandle = fopen('php://stdout', 'w');
+    }
+
+    public function writeLog(string $message):void {
+        $date = date('Y-m-d');
+
+        fwrite($this->fileHandle, "$date: $message\n");
+    }
+
+    public static function log(string $message): void {
+        $logger = static::getInstance();
+
+        $logger->writeLog($message);
+    }
+}
+
+class Config extends Singleton {
+    private $hashMap = [];
+
+    public function getValue(string $key) : string {
+        return $this->hashMap[$key];
+    }
+
+    public function setValue(string $key, string $value) : void {
+        $this->hashMap[$key] = $value;
+    }
+}
+
