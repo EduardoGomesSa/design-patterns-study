3f6632a84068c6519d839d02141a91cf48ff53b2
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-02-13 22:21:48 -0300

    Criação da interface renderer

--- StructuralPatterns/BridgeMethod/index.php
+++ StructuralPatterns/BridgeMethod/index.php
@@ -1,2 +1,123 @@
 <?php
 
+namespace RefactoringGuru\Bridge\RealWorld;
+
+abstract class Page
+{
+    protected $renderer;
+
+    public function __construct(Renderer $renderer)
+    {
+        $this->renderer = $renderer;
+    }
+
+    public function changeRenderer(Renderer $renderer): void
+    {
+        $this->renderer = $renderer;
+    }
+
+    abstract public function view(): string;
+}
+
+class SimplePage extends Page
+{
+    protected $title;
+    protected $content;
+
+    public function __construct(Renderer $renderer, string $title, string $content)
+    {
+        parent::__construct($renderer);
+        $this->title = $title;
+        $this->content = $content;
+    }
+
+    public function view(): string
+    {
+        return $this->renderer->renderParts([
+            $this->renderer->renderHeader(),
+            $this->renderer->renderTitle($this->title),
+            $this->renderer->renderTextBlock($this->content),
+            $this->renderer->renderer->renderFooter(),
+        ]);
+    }
+}
+
+
+class ProductPage extends Page
+{
+    protected $product;
+
+    public function __construct(Renderer $renderer, Product $product)
+    {
+        parent::__construct($renderer);
+        $this->product = $product;
+    }
+
+    public function view(): string
+    {
+        return $this->renderer->renderParts([
+            $this->renderer->renderHeader(),
+            $this->renderer->renderTitle($this->product->getTitle()),
+            $this->renderer->renderTextBlock($this->product->getDescription()),
+            $this->renderer->renderImage($this->product->getImage()),
+            $this->renderer->renderTextBlock('$' . number_format($this->product->getPrice(), 2)),
+            $this->renderer->renderLink("/cart/add/" . $this->product->getId(), "Add to cart"),
+            $this->renderer->renderFooter(),
+        ]);
+    }
+}
+
+class product
+{
+    private $id, $title, $description, $image, $price;
+
+    public function __construct(string $id, string $title, string $description, string $image, float $price)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->image = $image;
+        $this->price = $price;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
+
+interface Renderer {
+    public function renderTitle(string $title) : string;
+
+    public function renderTextBlock(string $text) : string;
+
+    public function renderImage(string $url) : string;
+
+    public function renderLink(string $url, string $title) : string;
+
+    public function renderHeader() : string;
+
+    public function renderFooter() : string;
+
+    public function renderParts(array $parts) : string;
+}
