6736fc07849ab90de757ca5eae1d897f103f3405
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-02-17 16:40:08 -0300

    criação da class Input

--- StructuralPatterns/CompositeMethod/index.php
+++ StructuralPatterns/CompositeMethod/index.php
@@ -2,6 +2,8 @@
 
 namespace RefactoringGuru\Composite\RealWorld;
 
+use function PHPSTORM_META\type;
+
 abstract class FormElement {
     protected $name;
     protected $title;
@@ -26,3 +28,20 @@ abstract class FormElement {
 
     abstract public function render() : string;
 }
+
+class Input extends FormElement {
+    private $type;
+
+    public function __construct(string $name, string $title, string $type) {
+        $this->name = $name;
+        $this->title = $title;
+        $this->type = $type;
+    }
+
+    public function render(): string
+    {
+        return "<label for=\"{$this->name}\">{$this->title}</label>\n" .
+            "<input name=\"{$this->name}\" type=\"{$this->type}\" value=\"{$this->data}\">\n";
+    }
+}
+
