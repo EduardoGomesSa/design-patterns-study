71692c1d5b2d5f9c9adb80d833392c235f9b543e
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-02-20 11:57:51 -0300

    Criação da concreta TextFormat

--- StructuralPatterns/DecoratorMethod/index.php
+++ StructuralPatterns/DecoratorMethod/index.php
@@ -1,3 +1,27 @@
 <?php
 
 namespace RefactoringGuru\Decorator\RealWorld;
+
+interface InputFormat {
+    public function formatText(string $text) : string;
+}
+
+class TextInput implements InputFormat {
+    public function formatText(string $text): string
+    {
+        return $text;
+    }
+}
+
+class TextFormat implements InputFormat {
+    protected $inputFormat;
+
+    public function __construct(InputFormat $inputFormat) {
+        $this->inputFormat = $inputFormat;
+    }
+
+    public function formatText(string $text): string
+    {
+        return $this->inputFormat->formatText($text);
+    }
+}
