334e95932f5019ee5a9ad697307701e24540b9c2
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-08 23:46:52 -0300

    feat: :sparkles: Finalização do exemplo conceitual de Flyweight

--- StructuralPatterns/FlyweightMethod/ConceptualExample/index.php
+++ StructuralPatterns/FlyweightMethod/ConceptualExample/index.php
@@ -2,39 +2,46 @@
 
 namespace RefactoringGuru\Flyweight\Conceptual;
 
-class Flyweight {
+class Flyweight
+{
     private $sharedState;
 
-    public function __construct($sharedState) {
+    public function __construct($sharedState)
+    {
         $this->sharedState = $sharedState;
     }
 
-    public function operation($uniqueState) : void {
+    public function operation($uniqueState): void
+    {
         $s = json_encode($this->sharedState);
         $u = json_encode($uniqueState);
         echo "Flyweight: Displaying shared ($s) and unique ($u) state.\n";
     }
 }
 
-class FlyweightFactory {
+class FlyweightFactory
+{
     private $flyweights = [];
 
-    public function __construct(array $initialFlyweights) {
-        foreach($initialFlyweights as $state) {
-            $this->flyweights[$this->getKey($state)] = new FlyweightFactory($state);
+    public function __construct(array $initialFlyweights)
+    {
+        foreach ($initialFlyweights as $state) {
+            $this->flyweights[$this->getKey($state)] = new Flyweight($state);
         }
     }
 
-    private function getKey(array $state): string {
+    private function getKey(array $state): string
+    {
         ksort($state);
 
         return implode("_", $state);
-    } 
+    }
 
-    public function getFlyweight(array $sharedState) : Flyweight {
+    public function getFlyweight(array $sharedState): Flyweight
+    {
         $key = $this->getKey($sharedState);
 
-        if(!isset($this->flyweights[$key])) {
+        if (!isset($this->flyweights[$key])) {
             echo "FlyweightFactory: Can`t find a flyweight, creating new one.\n";
             $this->flyweights[$key] = new Flyweight($sharedState);
         } else {
@@ -44,11 +51,55 @@ class FlyweightFactory {
         return $this->flyweights[$key];
     }
 
-    public function listFlyweights() : void {
+    public function listFlyweights(): void
+    {
         $count = count($this->flyweights);
         echo "\nFlyweightFactory: I have $count flyweights:\n";
-        foreach($this->flyweights as $key => $flyweight) {
+        foreach ($this->flyweights as $key => $flyweight) {
             echo $key . "\n";
         }
     }
 }
+
+$factory = new FlyweightFactory([
+    ["Chevrolet", "Camaro2018", "pink"],
+    ["Mercedes Benz", "C300", "black"],
+    ["Mercedes Benz", "C500", "red"],
+    ["BMW", "M5", "red"],
+    ["BMW", "X6", "white"],
+]);
+$factory->listFlyweights();
+
+function addCarToPoliceDatabase(
+    FlyweightFactory $ff,
+    $plates,
+    $owner,
+    $brand,
+    $model,
+    $color
+) {
+    echo "\nClient: Adding a car to database.\n";
+    $flyweight = $ff->getFlyweight([$brand, $model, $color]);
+
+    $flyweight->operation([$plates, $owner]);
+}
+
+addCarToPoliceDatabase(
+    $factory,
+    "CL234IR",
+    "James Doe",
+    "BMW",
+    "X1",
+    "red"
+);
+
+addCarToPoliceDatabase(
+    $factory,
+    "CL234IR",
+    "James Doe",
+    "BMW",
+    "X1",
+    "red"
+);
+
+$factory->listFlyweights();
