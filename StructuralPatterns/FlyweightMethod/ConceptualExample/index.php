2daf21861a313a2692db9aa1969cf8d8e554eb25
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-08 23:33:15 -0300

    feat: :sparkles: Criação da class Flyweight e FlyweightFactory

--- StructuralPatterns/FlyweightMethod/ConceptualExample/index.php
+++ StructuralPatterns/FlyweightMethod/ConceptualExample/index.php
@@ -1,3 +1,54 @@
 <?php
 
 namespace RefactoringGuru\Flyweight\Conceptual;
+
+class Flyweight {
+    private $sharedState;
+
+    public function __construct($sharedState) {
+        $this->sharedState = $sharedState;
+    }
+
+    public function operation($uniqueState) : void {
+        $s = json_encode($this->sharedState);
+        $u = json_encode($uniqueState);
+        echo "Flyweight: Displaying shared ($s) and unique ($u) state.\n";
+    }
+}
+
+class FlyweightFactory {
+    private $flyweights = [];
+
+    public function __construct(array $initialFlyweights) {
+        foreach($initialFlyweights as $state) {
+            $this->flyweights[$this->getKey($state)] = new FlyweightFactory($state);
+        }
+    }
+
+    private function getKey(array $state): string {
+        ksort($state);
+
+        return implode("_", $state);
+    } 
+
+    public function getFlyweight(array $sharedState) : Flyweight {
+        $key = $this->getKey($sharedState);
+
+        if(!isset($this->flyweights[$key])) {
+            echo "FlyweightFactory: Can`t find a flyweight, creating new one.\n";
+            $this->flyweights[$key] = new Flyweight($sharedState);
+        } else {
+            echo "Flyweight: Reusing existing flywight.\n";
+        }
+
+        return $this->flyweights[$key];
+    }
+
+    public function listFlyweights() : void {
+        $count = count($this->flyweights);
+        echo "\nFlyweightFactory: I have $count flyweights:\n";
+        foreach($this->flyweights as $key => $flyweight) {
+            echo $key . "\n";
+        }
+    }
+}
