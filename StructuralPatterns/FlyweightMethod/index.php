0a2f4e4d2bc3a3aa48d75267290ede9a0c8bffc6
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-12 14:15:51 -0300

    feat: :sparkles: finish example real world Flyweight

--- StructuralPatterns/FlyweightMethod/index.php
+++ StructuralPatterns/FlyweightMethod/index.php
@@ -55,14 +55,15 @@ class Cat
         $this->variation = $variation;
     }
 
-    public function matches(array $query): bool {
-        foreach($query as $key => $value) {
-            if(property_exists($this, $key)) {
-                if($this != $key) {
+    public function matches(array $query): bool
+    {
+        foreach ($query as $key => $value) {
+            if (property_exists($this, $key)) {
+                if ($this != $key) {
                     return false;
                 }
-            } elseif(property_exists($this->variation, $key)) {
-                if($this->variation->$key != $key) {
+            } elseif (property_exists($this->variation, $key)) {
+                if ($this->variation->$key != $key) {
                     return false;
                 }
             } else {
@@ -73,12 +74,13 @@ class Cat
         return true;
     }
 
-    // public function render() : string {
-    //     $this->variation->renderProfile($this->name, $this->age, $this->owner);
-    // }
+    public function render() {
+        $this->variation->renderProfile($this->name, $this->age, $this->owner);
+    }
 }
 
-class CatDataBase {
+class CatDataBase
+{
     private $cats = [];
     private $variations = [];
 
@@ -106,27 +108,74 @@ class CatDataBase {
         string $texture,
         string $fur,
         string $size
-    ) : CatVariation {
+    ): CatVariation {
         $key = $this->getKey(get_defined_vars());
 
-        if(!isset($this->variations[$key])) {
+        if (!isset($this->variations[$key])) {
             new CatVariation($breed, $image, $color, $texture, $fur, $size);
         }
 
         return $this->variations[$key];
     }
 
-    public function getKey(array $data) : string {
+    public function getKey(array $data): string
+    {
         return md5(implode("_", $data));
     }
 
-    public function findCat(array $query) {
-        foreach($this->cats as $cat) {
-            if($cat->matches($query)) {
+    public function findCat(array $query)
+    {
+        foreach ($this->cats as $cat) {
+            if ($cat->matches($query)) {
                 return $cat;
             }
         }
 
         echo "CatDatabase: Sorry, your query does not yield any results.";
     }
+}
+
+$db = new CatDataBase();
+
+echo "Client: letś see what we have in \"cats.csv\".\n";
+
+$handle = fopen(__DIR__ . "/cats.csv", "r");
+$row = 0;
+$columns = [];
+while (($data = fgetcsv($handle)) !== false) {
+    if ($row == 0) {
+        for ($c = 0; $c < count($data); $c++) {
+            $columnIndex = $c;
+            $columnKey = strtolower($data[$c]);
+            $columns[$columnKey] = $columnIndex;
+        }
+        $row++;
+        continue;
+    }
+
+    $db->addCat(
+        $data[$columns['name']],
+        $data[$columns['age']],
+        $data[$columns['owner']],
+        $data[$columns['breed']],
+        $data[$columns['image']],
+        $data[$columns['color']],
+        $data[$columns['texture']],
+        $data[$columns['fur']],
+        $data[$columns['size']],
+    );
+    $row++;
+}
+fclose($handle);
+
+echo "\nClient: letś look for a cat named \"Siri\".\n";
+$cat = $db->findCat(['name' => "Siri"]);
+if($cat) {
+    $cat->render();
+}
+
+echo "\nClient: letś look for a cat named \"Bob\".\n";
+$cat = $db->findCat(['name' => "Bob"]);
+if($cat) {
+    $cat->render();
 }
