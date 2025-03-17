daea1fa05acea5c78ef7c2ec68ce39b9e93e56af
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-17 15:07:03 -0300

    feat: :sparkles: finalização do exemplo de munndo real de ChainOfResponsibility

--- BehavioralPatterns/ChainOfResponsibilityMethod/index.php
+++ BehavioralPatterns/ChainOfResponsibilityMethod/index.php
@@ -1,1 +1,153 @@
 <?php
+
+namespace RefactoringGuru\ChainOfResponsibility\RealWorld;
+
+abstract class Middleware
+{
+    private $next;
+
+    public function linkWith(Middleware $next): Middleware
+    {
+        $this->next = $next;
+
+        return $next;
+    }
+
+    public function check(string $email, string $password): bool
+    {
+        if (!$this->next) {
+            return true;
+        }
+
+        return $this->next->check($email, $password);
+    }
+}
+
+class UserExistsMiddleware extends Middleware
+{
+    private $server;
+
+    public function __construct(Server $server)
+    {
+        $this->server = $server;
+    }
+
+    public function check(string $email, string $password): bool
+    {
+        if (!$this->server->hasEmail($email)) {
+            echo "UserExistsMiddleware: This email is not registered!\n";
+
+            return false;
+        }
+
+        if (!$this->server->isValidPassword($email, $password)) {
+            echo "UserExistsMiddleware: Wrong password!\n";
+
+            return false;
+        }
+
+        return parent::check($email, $password);
+    }
+}
+
+class RoleCheckMiddleware extends Middleware
+{
+    public function check(string $email, string $password): bool
+    {
+        if ($email === "admin@example.com") {
+            echo "RoleCheckMiddleware: Hello, admin!\n";
+
+            return true;
+        }
+
+        echo "RoleCheckMiddleware: Hello, user!\n";
+
+        return parent::check($email, $password);
+    }
+}
+
+class ThrottlingMiddleware extends Middleware
+{
+    private $requestPerMinute;
+    private $request;
+    private $currentTime;
+
+    public function __construct(int $requestPerMinute)
+    {
+        $this->requestPerMinute = $requestPerMinute;
+        $this->currentTime = time();
+    }
+
+    public function check(string $email, string $password): bool
+    {
+        if (time() > $this->currentTime + 60) {
+            $this->request = 0;
+            $this->currentTime = time();
+        }
+
+        $this->request++;
+
+        if ($this->request > $this->requestPerMinute) {
+            echo "ThrottlingMiddleware: Request limit exceeded!\n";
+            die();
+        }
+
+        return parent::check($email, $password);
+    }
+}
+
+class Server
+{
+    private $users = [];
+    private $middleware;
+
+    public function setMiddleware(Middleware $middleware): void
+    {
+        $this->middleware = $middleware;
+    }
+
+    public function logIn(string $email, string $password)
+    {
+        if ($this->middleware->check($email, $password)) {
+            echo "Server: Authorization has been successful!\n";
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function register(string $email, string $password): void
+    {
+        $this->users[$email] = $password;
+    }
+
+    public function hasEmail(string $email): bool
+    {
+        return isset($this->users[$email]);
+    }
+
+    public function isValidPassword(string $email, string $password): bool
+    {
+        return $this->users[$email] === $password;
+    }
+}
+
+$server = new Server();
+$server->register("admin@example.com", "admin_pass");
+$server->register("user@example.com", "user_pass");
+
+$middleware = new ThrottlingMiddleware(2);
+$middleware
+    ->linkWith(new UserExistsMiddleware($server))
+    ->linkWith(new RoleCheckMiddleware);
+
+$server->setMiddleware($middleware);
+
+do {
+    //echo "\nEnter your email:\n";
+    $email = readline("\nEnter your email:\n");
+    //echo "Enter your password:\n";
+    $password = readline("Enter your password:\n");
+    $success = $server->logIn($email, $password);
+} while (!$success);
