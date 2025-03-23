327d96d87bc002c4c5e589b98adf03ca05d0d3f8
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-22 23:47:28 -0300

    feat: :sparkles: Criação class SimpleCommand

--- BehavioralPatterns/CommandMethod/ConceptualExample/index.php
+++ BehavioralPatterns/CommandMethod/ConceptualExample/index.php
@@ -1,1 +1,20 @@
 <?php 
+
+namespace RefactoringGuru\Command\Conceptual;
+
+interface Command {
+    public function execute() : void;
+}
+
+class SimpleCommand implements Command {
+    private $payload;
+
+    public function __construct(string $payload) {
+        $this->payload = $payload;
+    }
+
+    public function execute(): void
+    {
+        echo "SimpleCommand: See, I can do simple things like printing (".$this->payload .")\n";
+    }
+}
