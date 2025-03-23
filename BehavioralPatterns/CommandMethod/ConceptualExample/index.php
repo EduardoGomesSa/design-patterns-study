1736e9de1b43db2cdbbcd4bc4ee9ca3fe1810fcb
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-03-22 23:55:15 -0300

    feat: :sparkles: Criação classe ComplexCommand e Receiver

--- BehavioralPatterns/CommandMethod/ConceptualExample/index.php
+++ BehavioralPatterns/CommandMethod/ConceptualExample/index.php
@@ -18,3 +18,32 @@ class SimpleCommand implements Command {
         echo "SimpleCommand: See, I can do simple things like printing (".$this->payload .")\n";
     }
 }
+
+class ComplexCommand implements Command {
+    private $receiver;
+    private $a;
+    private $b;
+
+    public function __construct(Receiver $receiver, string $a, string $b) {
+        $this->receiver = $receiver;
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function execute(): void
+    {
+        echo "ComplexCommand: Complex stuff should be done by a receiver object.\n";
+        $this->receiver->doSomething($this->a);
+        $this->receiver->doSomethingElse($this->b);
+    }
+}
+
+class Receiver {
+    public function doSomething(string $a) : void {
+        echo "Receiver: Working on(" . $a . ".)\n";
+    }
+
+    public function doSomethingElse(string $b) : void {
+        echo "Receiver: Also working on (" . $b . ".)\n";
+    }
+}
