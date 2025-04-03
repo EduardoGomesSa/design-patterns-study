9bd5bbb4fdb3e579ecc770cbd74a02519996c54c
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-04-03 14:44:04 -0300

    feat: :sparkles: Criação da class UserRepository

--- BehavioralPatterns/MediatorMethod/index.php
+++ BehavioralPatterns/MediatorMethod/index.php
@@ -53,12 +53,98 @@ class EventDispatcher
 }
 
 
-function events() : EventDispatcher {
+function events(): EventDispatcher
+{
     static $eventDispatcher;
 
-    if(!$eventDispatcher) {
+    if (!$eventDispatcher) {
         $eventDispatcher = new EventDispatcher();
     }
 
     return $eventDispatcher;
+}
+
+interface Observer
+{
+    public function update(string $event, object $emitter, $data = null);
+}
+
+class UserRepository implements Observer
+{
+    private $users = [];
+
+    public function __construct()
+    {
+        events()->attach($this, "users:deleted");
+    }
+
+    public function update(string $event, object $emitter, $data = null): void
+    {
+        switch ($event) {
+            case "users:deleted":
+                if ($emitter === $this) return;
+
+                $this->deleteUser($data, true);
+                break;
+        }
+    }
+
+    public function initialize(string $filename): void
+    {
+        echo "UserRepository: Loading user record from a file.\n";
+        events()->trigger("users:init", $this, $filename);
+    }
+
+    public function createUser(array $data, bool $silent = false): User
+    {
+        echo "UserRepository: Creating a user.\n";
+
+        $user = new User();
+        $user->update($data);
+
+        $id = bin2hex(openssl_random_pseudo_bytes(16));
+        $user->update(["id" => $id]);
+        $this->users[$id] = $user;
+
+        if (!$silent) {
+            events()->trigger("users:created", $this, $data);
+        }
+
+        return $user;
+    }
+
+    public function updateUser(User $user, array $data, bool $silent = false): User
+    {
+        echo "UserRepository: Updating a user.\n";
+
+        $id = $user->attributes["id"];
+        if (!isset($user[$id])) {
+            return null;
+        }
+
+        $user = $this->users[$id];
+        $user->update($data);
+
+        if (!$silent) {
+            events()->trigger("users:updated", $this, $user);
+        }
+
+        return $user;
+    }
+
+    public function deleteUser(User $user, bool $silent = false): void
+    {
+        echo "UserRepository: Deleting a user.\n";
+
+        $id = $user->attributes["id"];
+        if (!isset($user[$id])) {
+            return;
+        }
+
+        unset($this->users[$id]);
+
+        if (!$silent) {
+            events()->trigger("users:deleted", $this, $user);
+        }
+    }
 }
