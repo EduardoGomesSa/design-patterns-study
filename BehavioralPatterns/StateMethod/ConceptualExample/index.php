d7d9b7b3b52008e7556fba2a82fc90f097efe0fe
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-04-22 22:01:27 -0300

    feat: :sparkles: Criação da class Context e State

--- BehavioralPatterns/StateMethod/ConceptualExample/index.php
+++ BehavioralPatterns/StateMethod/ConceptualExample/index.php
@@ -1,3 +1,36 @@
 <?php
 
 namespace RefactoringGuru\Observer\RealWorld;
+
+class Context {
+    private $state;
+
+    public function __construct(State $state) {
+        $this->state = $state;
+    }
+
+    public function transitionTo(State $state) : void {
+        echo "Context: Transition to " . get_class($state) . ".\n";
+        $this->state = $state;
+        $this->state->setContext($this);
+    }
+
+    public function request1() : void {
+        $this->state->handle1();
+    }
+
+    public function request2() : void {
+        $this->state->handle2();
+    }
+}
+
+abstract class State {
+    protected $context;
+
+    public function setContext(Context $context) {
+        $this->context = $context;
+    }
+
+    abstract public function handle1() : void;
+    abstract public function handle2() : void;
+}
