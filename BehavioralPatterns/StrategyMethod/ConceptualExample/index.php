f417a330f01f3c945bfdacf61f9d01a5041736ec
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-04-27 19:20:51 -0300

    feat: :sparkles: Criação da class Context e interface Strategy

--- BehavioralPatterns/StrategyMethod/ConceptualExample/index.php
+++ BehavioralPatterns/StrategyMethod/ConceptualExample/index.php
@@ -1,3 +1,25 @@
 <?php
 
 namespace RefactoringGuru\Observer\Conceptual;
+
+Class Context {
+    private $strategy;
+
+    public function __construct(Strategy $strategy) {
+        $this->strategy = $strategy;
+    }
+
+    public function setStrategy(Strategy $strategy) {
+        $this->strategy = $strategy;
+    }
+
+    public function doSomeBusinessLogic() : void {
+        echo "Context: Sorting data using the strategy (not sure how it'll do it)\n";
+        $result = $this->strategy->doAlgorithm(["a","b","c","d","e",]);
+        echo implode(",", $result) . "\n";
+    }
+}
+
+interface Strategy {
+    public function doAlgorithm(array $data) : array;
+}
