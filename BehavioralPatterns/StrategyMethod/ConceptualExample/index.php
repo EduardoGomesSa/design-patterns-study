2bfb8903a5a6751232fd80bc35caae9ee79d683a
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-04-27 19:37:53 -0300

    feat: :sparkles: Finalização do exemplo conceitual de Strategy

--- BehavioralPatterns/StrategyMethod/ConceptualExample/index.php
+++ BehavioralPatterns/StrategyMethod/ConceptualExample/index.php
@@ -23,3 +23,31 @@ Class Context {
 interface Strategy {
     public function doAlgorithm(array $data) : array;
 }
+
+class ConcreteStrategyA implements Strategy {
+    public function doAlgorithm(array $data): array
+    {
+        sort($data);
+
+        return $data;
+    }
+}
+
+class ConcreteStrategyB implements Strategy {
+    public function doAlgorithm(array $data): array
+    {
+        rsort($data);
+
+        return $data;
+    }
+}
+
+$context = new Context(new ConcreteStrategyA());
+echo "Cient: Strategy is set to normal sorting.\n";
+$context->doSomeBusinessLogic();
+
+echo "\n";
+
+echo "Client: Strategy is set to reverse sorting.\n";
+$context->setStrategy(new ConcreteStrategyB());
+$context->doSomeBusinessLogic();
