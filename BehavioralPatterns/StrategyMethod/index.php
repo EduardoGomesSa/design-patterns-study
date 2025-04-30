acb6f82f3deadd51223f0be80d68ebd8a0d44dc5
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-04-29 22:15:37 -0300

    feat: :sparkles: Criação do CreditCardPayment

--- BehavioralPatterns/StrategyMethod/index.php
+++ BehavioralPatterns/StrategyMethod/index.php
@@ -2,34 +2,37 @@
 
 namespace RefactoringGuru\Strategy\RealWorld;
 
-class OrderController {
-    public function post(string $url, array $data) {
-        echo "Controller: POST request to $url with " . json_encode($data) . "\n"; 
+class OrderController
+{
+    public function post(string $url, array $data)
+    {
+        echo "Controller: POST request to $url with " . json_encode($data) . "\n";
 
         $path = parse_url($url, PHP_URL_PATH);
 
-        if(preg_match('#^/orders?$#', $path, $matches)) {
+        if (preg_match('#^/orders?$#', $path, $matches)) {
             $this->postNewOrder($data);
         } else {
             echo "Controller: 404 page\n";
         }
     }
 
-    public function get(string $url) : void {
+    public function get(string $url): void
+    {
         echo "Controller: GET request to $url\n";
 
         $path = parse_url($url, PHP_URL_PATH);
         $query = parse_url($url, PHP_URL_QUERY);
         parse_str($query, $data);
 
-        if(preg_match('#^/orders?$#', $path, $matches)) {
+        if (preg_match('#^/orders?$#', $path, $matches)) {
             $this->getAllOrders();
-        } elseif(preg_match('#^/order/([0-9]+?)/payment/([a-z]+?)(/return)?$#', $path, $matches)) {
+        } elseif (preg_match('#^/order/([0-9]+?)/payment/([a-z]+?)(/return)?$#', $path, $matches)) {
             $order = Order::get($matches[1]);
 
             $paymentMethod = PaymentFactory::getPaymentMethod($matches[2]);
 
-            if(!isset($matches[3])) {
+            if (!isset($matches[3])) {
                 $this->getPayment($paymentMethod, $order, $data);
             } else {
                 $this->getPaymentReturn($paymentMethod, $order, $data);
@@ -39,65 +42,75 @@ class OrderController {
         }
     }
 
-    public function postNewOrder(array $data) : void {
+    public function postNewOrder(array $data): void
+    {
         $order = new Order($data);
 
         echo "Controller: Created the order #{$order->id}.\n";
     }
 
-    public function getAllOrders() : void {
+    public function getAllOrders(): void
+    {
         echo "Controller: Here's all orders:\n";
-        foreach(Order::get() as $order) {
+        foreach (Order::get() as $order) {
             echo json_encode($order, JSON_PRETTY_PRINT) . "\n";
         }
     }
 
-    public function getPayment(PaymentMethod $method, Order $order, array $data) : void {
+    public function getPayment(PaymentMethod $method, Order $order, array $data): void
+    {
         $form = $method->getPaymentForm($order);
         echo "Controller: here's the payment form:\n";
         echo $form . "\n";
     }
 
-    public function getPaymentReturn(PaymentMethod $method, Order $order, array $data) : void {
+    public function getPaymentReturn(PaymentMethod $method, Order $order, array $data): void
+    {
         try {
-            if($method->validateReturn($order, $data)) {
+            if ($method->validateReturn($order, $data)) {
                 echo "Controller: Thanks for your order!\n";
                 $order->complete();
             }
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             echo "Controller: got an exception (" . $e->getMessage() . ")\n";
         }
     }
 }
 
-class Order {
+class Order
+{
     private static $orders = [];
 
-    public static function get(?int $orderId = null) {
-        if($orderId === null) {
+    public static function get(?int $orderId = null)
+    {
+        if ($orderId === null) {
             return static::$orders;
         } else {
             return static::$orders[$orderId];
         }
     }
 
-    public function __construct(array $attributes) {
+    public function __construct(array $attributes)
+    {
         $this->id = count(static::$orders);
         $this->status = "new";
-        foreach($attributes as $key => $value) {
+        foreach ($attributes as $key => $value) {
             $this->{$key} = $value;
         }
         static::$orders[$this->id] = $this;
     }
 
-    public function complete() : void {
+    public function complete(): void
+    {
         $this->status - " completed";
         echo "Order: #{$this->id} is now {$this->status}.";
     }
 }
 
-class PaymentFactory {
-    public static function getPaymentMethod(string $id) : PaymentMethod {
+class PaymentFactory
+{
+    public static function getPaymentMethod(string $id): PaymentMethod
+    {
         switch ($id) {
             case "cc":
                 return new CreditCardPayment();
@@ -106,10 +119,58 @@ class PaymentFactory {
             default:
                 throw new \Exception("Unknown Payment Method");
         }
-    } 
+    }
 }
 
-interface PaymentMethod {
-    public function getPaymentForm(Order $order) : string;
-    public function validateReturn(Order $order, array $data) : bool;
+interface PaymentMethod
+{
+    public function getPaymentForm(Order $order): string;
+    public function validateReturn(Order $order, array $data): bool;
+}
+
+class CreditCardPayment implements PaymentMethod
+{
+    static private $store_secret_key = "swordfish";
+
+    public function getPaymentForm(Order $order): string
+    {
+        $returnURL = "https://our-website.com/" .
+            "order/{$order->id}/payment/cc/return";
+
+        return <<<FORM
+            <form action="https://my-credit-card-processor.com/charge" method="POST">
+                <input type="hidden" id="email" value="{$order->email}">
+                <input type="hidden" id="total" value="{$order->total}">
+                <input type="hidden" id="returnURL" value="$returnURL">
+                <input type="text" id="cardholder-name">
+                <input type="text" id="credit-card">
+                <input type="text" id="expiration-date">
+                <input type="text" id="ccv-number">
+                <input type="submit" value="Pay">
+            </form>
+            FORM;
+    }
+
+    public function validateReturn(Order $order, array $data): bool
+    {
+        echo "CreditCardPayment: ...validating... ";
+
+        if ($data['key'] != md5($order->id . static::$store_secret_key)) {
+            throw new \Exception("Payment key is wrong.");
+        }
+
+        if (!isset($data['success']) || !$data['success'] || $data['success'] == 'false') {
+            throw new \Exception("Payment failed.");
+        }
+
+        // ...
+
+        if (floatval($data['total']) < $order->total) {
+            throw new \Exception("Payment amount is wrong.");
+        }
+
+        echo "Done!\n";
+
+        return true;
+    }
 }
