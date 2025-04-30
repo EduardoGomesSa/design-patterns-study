7fc6da4a6f5c0d1c9a5a1484e94d9091fdf3642f
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-04-29 21:55:01 -0300

    feat: :sparkles: criação da interface PaymentMethod

--- BehavioralPatterns/StrategyMethod/index.php
+++ BehavioralPatterns/StrategyMethod/index.php
@@ -108,3 +108,8 @@ class PaymentFactory {
         }
     } 
 }
+
+interface PaymentMethod {
+    public function getPaymentForm(Order $order) : string;
+    public function validateReturn(Order $order, array $data) : bool;
+}
