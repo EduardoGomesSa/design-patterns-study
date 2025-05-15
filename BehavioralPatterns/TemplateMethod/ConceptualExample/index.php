4bb3a2b45fef0d3d07cea594b230286f837f2d98
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-14 22:51:49 -0300

    feat: :sparkles: Criação classes concretas

--- BehavioralPatterns/TemplateMethod/ConceptualExample/index.php
+++ BehavioralPatterns/TemplateMethod/ConceptualExample/index.php
@@ -31,3 +31,32 @@ abstract class AbstractClass {
     protected function hook1() : void {}
     protected function hook2() : void {}
 }
+
+class ConcreteClass1 extends AbstractClass {
+    protected function requiredOperations1(): void
+    {
+        echo "ConcreteClass1 says: Implemented Operation1\n";
+    }
+
+    protected function requiredOperations2(): void
+    {
+        echo "ConcreteClass1 says: Implemented Operation2\n";
+    }
+}
+
+class ConcreteClass2 extends AbstractClass {
+    protected function requiredOperations1(): void
+    {
+        echo "ConcreteClass2 says: Implemented Operation1\n";
+    }
+
+    protected function requiredOperations2(): void
+    {
+        echo "ConcreteClass2 says: Implemented Operation2\n";
+    }
+
+    protected function hook1(): void
+    {
+        echo "ConcreteClass2 says: Overridden Hook1\n";
+    }
+}
