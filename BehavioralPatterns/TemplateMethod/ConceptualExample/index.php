7900b91d9832344a1a3d967e1bbd7a6d3025db87
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-14 22:34:04 -0300

    feat: :sparkles: criação da class AbstractClass

--- BehavioralPatterns/TemplateMethod/ConceptualExample/index.php
+++ BehavioralPatterns/TemplateMethod/ConceptualExample/index.php
@@ -1,2 +1,33 @@
 <?php
 
+namespace RefactoringGuru\TemplateMethod\Conceptual;
+
+abstract class AbstractClass {
+    final public function templateMethod() : void {
+        $this->baseOperation1();
+        $this->requiredOperations1();
+        $this->baseOperation2();
+        $this->hook1();
+        $this->requiredOperations2();
+        $this->baseOperation3();
+        $this->hook2();
+    }
+
+    protected function baseOperation1() : void {
+        echo "AbstractClass says: I'm doing the bulk of the work\n";
+    }
+
+    protected function baseOperation2() : void {
+        echo "AbstractClass says: But I let subclasses override some operations\n";
+    }
+
+    protected function baseOperation3() : void {
+        echo "AbstractClass says: But I am doing the bulk of the work anyway\n";
+    }
+
+    abstract protected function requiredOperations1() : void;
+    abstract protected function requiredOperations2() : void;
+
+    protected function hook1() : void {}
+    protected function hook2() : void {}
+}
