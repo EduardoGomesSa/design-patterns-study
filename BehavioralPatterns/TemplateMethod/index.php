ecc9a92e8bf3609dbad784b38e0815aa702f953b
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-16 23:57:27 -0300

    feat: :sparkles: criação class SocialNetwork

--- BehavioralPatterns/TemplateMethod/index.php
+++ BehavioralPatterns/TemplateMethod/index.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace RefactoringGuru\TemplateMethod\RealWorld;
+
+abstract class SocialNetwork {
+    protected $username;
+    protected $password;
+
+    public function __construct(string $username, string $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
+}
