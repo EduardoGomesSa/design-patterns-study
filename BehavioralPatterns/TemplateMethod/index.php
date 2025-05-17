e39022b1625836945c76b11d23fb37c6675dd814
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-05-17 20:29:40 -0300

    feat: :sparkles: finalização class SocialNetwork

--- BehavioralPatterns/TemplateMethod/index.php
+++ BehavioralPatterns/TemplateMethod/index.php
@@ -10,4 +10,20 @@ abstract class SocialNetwork {
         $this->username = $username;
         $this->password = $password;
     }
+
+    public function post(string $message) : bool {
+        if($this->logIn($this->username, $this->password)) {
+            $result = $this->sendData($message);
+
+            $this->logOut();
+
+            return $result;
+        }
+
+        return false;
+    }
+
+    abstract public function logIn(string $username, string $password) : bool;
+    abstract public function sendData(string $message) : bool;
+    abstract public function logOut() : void;
 }
