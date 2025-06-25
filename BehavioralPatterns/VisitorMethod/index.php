854087648f4cea49eb56333467a2500816d12f0b
Eduardo Sa <eduardo.sa.022@gmail.com>
2025-06-24 22:19:33 -0300

    feat: :sparkles: Interfaces and concrete class

--- BehavioralPatterns/VisitorMethod/index.php
+++ BehavioralPatterns/VisitorMethod/index.php
@@ -1,1 +1,109 @@
-<?php 
+<?php
+
+namespace RefactoringGuru\Visitor\RealWorld;
+
+interface Entity
+{
+    public function accept(Visitor $visitor): string;
+}
+
+class Company implements Entity
+{
+    private $name;
+    private $departments;
+
+    public function __construct(string $name, array $departments)
+    {
+        $this->name = $name;
+        $this->departments = $departments;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDepartments(): array
+    {
+        return $this->departments;
+    }
+
+    public function accept(Visitor $visitor): string
+    {
+        return $visitor->visitCompany($this);
+    }
+}
+
+class Department implements Entity
+{
+    private $name;
+    private $employees;
+
+    public function __construct(string $name, array $employees)
+    {
+        $this->name = $name;
+        $this->employees = $employees;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmployees(): array
+    {
+        return $this->employees;
+    }
+
+    public function getCost(): int
+    {
+        $cost = 0;
+
+        foreach ($this->employees as $employee) {
+            $cost += $employee->getSalary();
+        }
+
+        return $cost;
+    }
+
+    public function accept(Visitor $visitor): string
+    {
+        return $visitor->visitDepartment($this);
+    }
+}
+
+class Employee implements Entity {
+    private $name;
+    private $position;
+    private $salary;
+
+    public function __construct(string $name, string $position, int $salary) {
+        $this->name = $name;
+        $this->position = $position;
+        $this->salary = $salary;
+    }
+
+    public function getName(): string{
+        return $this->name;
+    }
+
+    public function getPosition(): string {
+        return $this->position;
+    }
+
+    public function getSalary(): int {
+        return $this->salary;
+    }
+
+    public function accept(Visitor $visitor): string
+    {
+        return $visitor->visitEmployee($this);
+    }
+}
+
+interface Visitor
+{
+    public function visitCompany(Company $company): string;
+    public function visitDepartment(Department $department): string;
+    public function visitEmployee(Employee $employee): string;
+}
